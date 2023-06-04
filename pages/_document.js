843dd35f37a3e307ed85e4b97edb7cd549e7a90b
Namaku1801 <namaku1801@upi.edu>
2023-06-04 18:02:46 +0700

    edit tag head

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Tugas Basis Data (Kelompok 1)"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        // <meta name="description" content="Tugas Basis Data (Kelompok 1)"/>
+        // <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        // <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
