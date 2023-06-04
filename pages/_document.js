418b8eac2811e5de1d053191101ccbf9760acf9a
Namaku1801 <namaku1801@upi.edu>
2023-06-04 17:19:59 +0700

    add meta

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Tugas Basis Data (Kelompok 1)"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
