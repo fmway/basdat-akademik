8d4bcfef1be49e4551f2c6f14bfff2533c61c942
Namaku1801 <namaku1801@upi.edu>
2023-06-04 17:37:13 +0700

    edit tag head

--- pages/index.js
+++ pages/index.js
@@ -56,10 +56,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Tabel Dosen</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Tugas Basis Data (Kelompok 1)"/>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Tugas Basis Data (Kelompok 1)"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <title>Tabel Dosen</title>
       </Head>
       <div className={styles.body}>
         <Popup open={openModal} onClose={closeModal} closeOnDocumentClick position="top center" contentStyle={{marginTop: 0}}>
