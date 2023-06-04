6ebb5838e7e3cffa864fd0585fc659d2cda6eb8e
Namaku1801 <namaku1801@upi.edu>
2023-06-04 13:17:49 +0700

    fix loading form

--- pages/form/[nip].js
+++ pages/form/[nip].js
@@ -110,9 +110,10 @@ export default function Form() {
       </Head>
       <div className={styles.body}>
         <div className={styles.container}>
-          <div className={styles.title}>Edit Dosen
+          <div className={styles.progress}>
             <ProgressBar height="50" width="45" visible={loadDosen} ariaLabel="progress-bar-loading" wrapperStyle={{}} wrapperClass="progress-bar-wrapper" borderColor = '#71b7e6' barColor = '#9b59b6' />
-            {!loadDosen && <Toggle toggled={edit} setToggled={setEdit} />}
+          </div>
+          <div className={styles.title}>Edit Dosen {!loadDosen && <Toggle toggled={edit} setToggled={setEdit} />}
           </div>
           <form onSubmit={handleSubmit}>
             <div className={styles.userDetails}>
